62c00a973cb149301d02eadb5a0f298262cf2e40
shreekshitha <shreekshitha@gmail.com>
2017-01-16 13:26:58 +0530

    Adding home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'page/home'
+
+  get 'page/about'
+
+  get 'page/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
